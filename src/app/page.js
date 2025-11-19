8520983916da243b68cd26d6ca499d533f429213
sultan9723 <sultan.malik9723@gmail.com>
2025-11-19 13:12:40 +0500

    Revert "carasoul feature Updated"
    This reverts commit ce7f5b4f44474b0d0e95d2d6da81dee301893918.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,5 @@
 import HeroVideo from "@/components/HeroVideo";
-import Carousel from "@/components/Carousel";
 
 export default function Home() {
-  return (
-    <>
-      <HeroVideo />
-      <Carousel />
-    </>
-  );
+  return <HeroVideo />;
 }
