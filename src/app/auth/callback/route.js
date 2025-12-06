29cd0e36099cdff641f198f5182be21f28193645
sultan9723 <sultan.malik9723@gmail.com>
2025-12-06 20:07:53 +0500

    Fok Updates

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -1,15 +1,13 @@
 import { NextResponse } from "next/server";
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 
-export async function GET(request) {
-  const { searchParams } = new URL(request.url);
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
   const code = searchParams.get("code");
-
   if (code) {
-    const supabase = await createSupabaseServerClient();
+    const supabase = createSupabaseServerClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
-
   return NextResponse.redirect(
     `${process.env.NEXT_PUBLIC_SITE_URL}/dashboard`
   );
